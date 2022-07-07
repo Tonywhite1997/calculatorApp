abe6e21481cc397ceb972eb49c48ba2e7734f351
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2022-07-07 10:44:40 +0100

    made more improvement

--- app.js
+++ app.js
@@ -8,30 +8,92 @@ const euqaulBtn = document.querySelector(".equals__to")
 const clearBtn = document.querySelector(".clear")
 const deleteBtn = document.querySelector(".fa-delete-left")
 
-
+let clickCounter = 0;
 keys.forEach((key)=>{
     key.addEventListener("click", function(){
-        let {value} = this.dataset;
-        inputField.value += value.toLocaleString();
-    })
-})
+        resultField.value = "";
+        let{value} = this.dataset;
 
-euqaulBtn.addEventListener("click", ()=>{
-    console.log(inputField.value);
-    resultField.value = eval(inputField.value).toLocaleString();
-})
+        if(value === "()"){
+            clickCounter += 1
 
-clearBtn.addEventListener("click", ()=>{
+            if(clickCounter < 0){
+                clickCounter = 1;
+            }
+
+            if(clickCounter === 1){
+                value = "("
+            }
+            if(clickCounter === 2){
+                value = ")"
+                clickCounter = 0;
+            }
+            if(clickCounter > 2){
+                return;
+            }
+        }
+
+        if(value === "^"){
+            value = "**";
+        };
+
+        if(inputField.value === "Error"){
+            inputField.value = ""
+        }
+        inputField.value += value;
+
+        if(value==="1"||value==="2"||value==="3"||value==="4"||value==="5"||value==="6"||value==="7"||value==="8"||value==="9"||value==="0"){
+            resultField.value = "";
+        }
+
+    });
+});
+
+function clearInputField(){
     inputField.value = "";
     resultField.value = "";
-})
+    clickCounter = 0;
+}
 
-deleteBtn.addEventListener("click", ()=>{
-    let inputValue
-    let numberArray = [...inputField.value]
+function deleteLastInput(){
+    if(inputField.value === ""){
+        return;
+    };
+
+    if(inputField.value === "Error"){
+        return inputField.value = "";
+    }
+
+    let inputValue;
+    let numberArray = [...inputField.value];
+
+    if(numberArray.length ===  1){
+        return inputField.value = "";
+    }
+    
     let afterDeleteOp = numberArray.slice(0, -1);
     for(number of afterDeleteOp){
+        if(number === "(" || ")"){
+            clickCounter = 1
+        }
         inputValue = afterDeleteOp.join("")
     }
     inputField.value = inputValue;
+}
+
+euqaulBtn.addEventListener("click", ()=>{
+    if(inputField.value === ""){
+        return
+    }
+
+    if(inputField.value[0] === "*"|| inputField.value[0] === "%" || inputField.value[0] === "/" || inputField.value[0] === "^"){
+        return inputField.value = "Error"
+    }
+
+    resultField.value = eval(inputField.value).toLocaleString();
+    inputField.value = "";
 })
+
+clearBtn.addEventListener("click", clearInputField)
+
+deleteBtn.addEventListener("click", deleteLastInput)
